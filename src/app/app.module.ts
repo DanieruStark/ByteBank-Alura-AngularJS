15638eca4f9840f0df04ac7ee8ccf60719255db4
Daniel Santana <danielmaster19@gmail.com>
2022-08-23 07:01:41 -0300

    criado opção de extrato

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NovaTranferenciaComponent } from './components/nova-tranferencia/nova-tranferencia/nova-tranferencia.component';
 import { ExtratoComponent } from './components/extrato/extrato.component';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+
+registerLocaleData(localePt, 'pt');
 
 @NgModule({
   declarations: [
@@ -15,7 +19,10 @@ import { ExtratoComponent } from './components/extrato/extrato.component';
   imports: [
     BrowserModule, FormsModule
   ],
-  providers: [],
+  providers: [
+    {provide: LOCALE_ID, useValue: 'pt'},
+    {provide: DEFAULT_CURRENCY_CODE, useValue: 'BRL'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
